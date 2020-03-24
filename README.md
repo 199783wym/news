29061a7428dcddd7c21244914a4e74707ec83ed4
wangym <642688028@qq.com>
2020-03-25 00:50:31 +0800

    添加了逆向工程配置文件 数据库配置 readme  准备开始逆向工程

---
+++ README.md
@@ -0,0 +1,8 @@
+##雁鸣社区
+
+##资料
+
+##工具
+
+### 逆向工程覆盖生产
+mvn -Dmybatis.generator.overwrite=true mybatis-generator:generate
--- news-worker/src/main/java/news/newsworker/controller/LoginController.java
+++ news-worker/src/main/java/news/newsworker/controller/LoginController.java
@@ -2,10 +2,13 @@ package news.newsworker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @Author: ym
@@ -20,8 +23,19 @@ public class LoginController {
             @RequestParam(value = "password",required =false)String password,
             HttpServletRequest request,
             Model model
-    ){
+    ) throws NoSuchAlgorithmException {
         System.out.println(username+password);
+//        MessageDigest md = MessageDigest.getInstance("MD5");
+//        // java自带工具包MessageDigest
+////        String resultString = MD5Utils.md5("123456");
+//        System.out.println(resultString);
+//        // e10adc3949ba59abbe56e057f20f883e
+//        String resultString1 = MD5Utils.md5("1234");
+//        System.out.println(resultString1);
+//        //81dc9bdb52d04dc20036dbd8313ed055
+//
+//        // spring自带工具包DigestUtils
+        System.out.println(DigestUtils.md5DigestAsHex("1234".getBytes()));
        return "index";
     }
 
