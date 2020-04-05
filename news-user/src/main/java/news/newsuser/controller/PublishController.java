c45a2d7048fec955ba3858a7fef37202420f767b
wangym <642688028@qq.com>
2020-04-05 23:15:54 +0800

    调整发布线索页面背景和颜色 修改导航条的字体和颜色

---
+++ news-user/src/main/java/news/newsuser/controller/PublishController.java
@@ -0,0 +1,19 @@
+package news.newsuser.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Author: ym
+ * @Date: 2020/4/5 23:12
+ * @Version 1.0
+ */
+@Controller
+public class PublishController {
+
+    @GetMapping(value="/publish")
+    public String toPublish(){
+        return "publish";
+
+    }
+}
