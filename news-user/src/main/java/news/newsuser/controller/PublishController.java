12c300090e4c92136af913ac06069ef45c493bc1
wangym <642688028@qq.com>
2020-04-07 23:36:17 +0800

    从前端接收线索数据

--- news-user/src/main/java/news/newsuser/controller/PublishController.java
+++ news-user/src/main/java/news/newsuser/controller/PublishController.java
@@ -1,13 +1,18 @@
 package news.newsuser.controller;
 
 import news.newsuser.mapper.DictionaryMapper;
+import news.newsuser.model.Clue;
 import news.newsuser.model.Dictionary;
+import news.newsuser.model.User;
 import news.newsuser.service.PublishService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -27,5 +32,51 @@ public class PublishController {
         List<Dictionary> clueTypes= publishService.search(1L);
         model.addAttribute("ClueTypes", clueTypes);
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "type", required = false) String type,
+            @RequestParam(value = "id", required = false) Long id,
+            HttpServletRequest request,
+            Model model) {
+        //查字典
+        List<Dictionary> clueTypes= publishService.search(1L);
+        model.addAttribute("ClueTypes", clueTypes);
+
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("type", type);
+
+        if (title == null || title == "") {
+            model.addAttribute("error", "标题不能为空");
+            return "redirect:/publish";
+        }
+        if (description == null || description == "") {
+            model.addAttribute("error", "内容不能为空");
+            return "redirect:/publish";
+        }
+        if (type == null || type == "") {
+            model.addAttribute("error", "类型不能为空");
+            return "redirect:/publish";
+        }
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "redirect:/publish";
+        }
+//        Question question = new Question();
+//        question.setTitle(title);
+//        question.setDescription(description);
+//        question.setTag(tag);
+//        question.setCreator(user.getId());
+//        question.setId(id);
+//        questionService.createOrUpdate(question);
+        //根据type值
+        Clue clue=new Clue();
+
+        return "redirect:/";
     }
 }
