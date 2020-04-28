cbf7675f38eef83695a792a1f772b8535ad644b6
wangym <642688028@qq.com>
2020-04-28 23:25:13 +0800

    修改user前端页面  增加手机号

---
+++ news-user/src/main/resources/static/js/publish.js
@@ -0,0 +1,55 @@
+function submitForm(pageIndex, pageSize) {
+    var formData = new FormData(); //将需要提交的参数封装起来
+    formData.append("id", $("#id").val());
+    var zswb = $("#file").val();    //获取file中的内容,看是否有值
+    if (zswb == '' || zswb.length < 1) {    //没有file提交的时候走的接口
+        $.ajax({
+            url : '/editMovieWithoutFile',
+            type : 'post',
+            data : formData,
+            processData : false,
+            contentType : false,
+            success : function(value) {
+                var result = JSON.parse(value);
+                if (result == 'true') {
+                    window.location.href = "/index?pageIndex=" + pageIndex+ "&pageSize=" + pageSize;
+                } else {
+                    Lobibox.alert('error', {msg : "媒资信息更新失败!!!"});
+                }
+            }
+        });
+    } else {    //有file提交的时候走的接口
+        formData.append("file", $("#file")[0].files[0]);
+        $.ajax({
+            url : '/editMovieInfo',
+            type : 'post',
+            data : formData,
+            processData : false,
+            contentType : false,
+            success : function(value) {
+                var result = JSON.parse(value);
+                if (result == 'true') {
+                    window.location.href = "/index?pageIndex=" + pageIndex+ "&pageSize=" + pageSize;
+                } else {
+                    Lobibox.alert('error', {msg : "媒资信息更新失败!!!"});
+                }
+            }
+        });
+    }
+}
+
+//图片回显:
+function preview(file) {
+    $("#imgHidden").css("display", "none");
+    var prevDiv = document.getElementById('preview');
+    if (file.files && file.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(evt) {
+            prevDiv.innerHTML = '<img style="width: 100px;height: 100px;" src="' + evt.target.result + '" />';
+        }
+        reader.readAsDataURL(file.files[0]);
+    } else {
+        prevDiv.innerHTML = '<div class="img" style="width: 100px;height:100px;filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale,src=\'' +
+            file.value + '\'"></div>';
+    }
+}
